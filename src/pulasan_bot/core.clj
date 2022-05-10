29227b4c60cbef646405e6ea3db95b46cb6bb643
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-05-10 15:03:11 +0800

    add handler for simple question

--- src/pulasan_bot/core.clj
+++ src/pulasan_bot/core.clj
@@ -7,27 +7,34 @@
             [morse.api :as t])
   (:gen-class))
 
-; TODO: fill correct token
 (def token (env :telegram-token))
 
+(defn respond-to-baobao [text]
+  (when-let [[match] (re-seq #"宝宝(.+)不\1" text)]
+    (str (second match) "!!!")))
+
+(def c (atom nil))
 
 (h/defhandler handler
 
   (h/command-fn "start"
-    (fn [{{id :id :as chat} :chat}]
-      (println "Bot joined new chat: " chat)
-      (t/send-text token id "Welcome to pulasan-bot!")))
+                (fn [{{id :id :as chat} :chat}]
+                  (println "Bot joined new chat: " chat)
+                  (t/send-text token id "Welcome to pulasan-bot!")))
 
   (h/command-fn "help"
-    (fn [{{id :id :as chat} :chat}]
-      (println "Help was requested in " chat)
-      (t/send-text token id "Help is on the way")))
+                (fn [{{id :id :as chat} :chat}]
+                  (println "Help was requested in " chat)
+                  (t/send-text token id "Help is on the way")))
 
   (h/message-fn
-    (fn [{{id :id} :chat :as message}]
-      (println "Intercepted message: " message)
-      (t/send-text token id "I don't do a whole lot ... yet."))))
+   (fn [{{id :id} :chat :as message}]
+     (reset! c message)
+     (println "Intercepted message: " message)
+     (t/send-text token id (or (respond-to-baobao (:text message)) "I don't do a whole lot ... yet.")))))
 
+;; start bot in repl
+(def channel (p/start token handler))
 
 (defn -main
   [& args]
@@ -36,4 +43,9 @@
     (System/exit 1))
 
   (println "Starting the pulasan-bot")
-  (<!! (p/start token handler)))
+  (<!! channel))
+
+(comment
+  ;; stop bot in repl
+  (p/stop channel)
+  @c)
