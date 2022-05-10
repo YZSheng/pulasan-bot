a6c0c0941a0903202016d6aee10dc45c35b15cc3
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-05-10 14:12:56 +0800

    update to lein 2.0

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [morse               "0.2.4"]]
 
   :plugins [[lein-environ "1.1.0"]]
-
+  :min-lein-version "2.0.0"
   :main ^:skip-aot pulasan-bot.core
   :target-path "target/%s"
   :uberjar-name "pulasan-bot-standalone.jar"
